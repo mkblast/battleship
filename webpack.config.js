3a7d140c4e2cb5a15043f942385e9fd213ba43d6
Karim Mk <mk47blast@protonmail.com>
2024-01-18 17:43:58 +0100

    Adding the development mode

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,13 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  mode: "development",
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+  },
 };
